e69dd0d52cd8062582b1fdb138836dd835d09be9
bissaron <6310210224@psu.ac.th>
2023-07-13 19:54:56 +0700

    lab2 getall,create Employee

--- src/main/java/com/example/demo/BackendJavaApplication.java
+++ src/main/java/com/example/demo/BackendJavaApplication.java
@@ -9,7 +9,8 @@ public class BackendJavaApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(BackendJavaApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello spring");
+		System.out.println("Spring boot");
 	}
 
 }
---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskExecutor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+
+@RestController
+public class EmployeeController {
+	
+	private List<Employee> data = new ArrayList<Employee>();
+	@GetMapping("/employee")
+	public List<Employee> getEmployee(){
+		return data;
+	}
+	
+	@PostMapping("/employee")
+	public Employee addEmployee(@RequestBody Employee body) {
+		for(int i = 0; i< data.size(); i++) {
+			if(body.getEmployeeId() == data.get(i).getEmployeeId() ) {
+				return null;
+				
+			}
+				
+		}
+		data.add(body);
+		return body;
+			
+	}
+	
+
+}
