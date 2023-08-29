57a44b4250f8fbfabf1713779c25608e58c05244
bissaron <6310210224@psu.ac.th>
2023-08-29 14:23:09 +0700

    Exam1

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,9 +3,10 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Employee;
+
+import com.example.demo.model.Student;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface StudentRepository extends JpaRepository<Student, Integer>{
 
 }
